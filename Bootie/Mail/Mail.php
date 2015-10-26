bf00eff20df2f07b1b81f42dae10fc8d84b3870b
MartinFrith <telemagico@gmail.com>
2015-10-26 10:27:33 -0300

    Devel firs commit

---
+++ Bootie/Mail/Mail.php
@@ -0,0 +1,52 @@
+<?php namespace Bootie;
+
+class Mailer extends \Bootie\PHPMailerLite {
+
+	public function send($recipient,$subject,$tpl,$data=array(),$from="")
+	{
+
+		if( in_array($_SERVER['REMOTE_ADDR'],['127.0.0.1'])) return false;
+
+		$message = template($tpl,$data);
+
+		//Create a new PHPMailer instance
+		$mail = new \Bootie\PHPMailerLite;
+		// Set PHPMailer to use the sendmail transport
+		$mail->isSendmail();
+		//Set who the message is to be sent from
+		$mail->setFrom('noreply@devmeta.net', 'Devmeta Blogs');
+		//Set an alternative reply-to address
+		$mail->addReplyTo('noreply@devmeta.net', 'Devmeta Blogs');
+		//Set who the message is to be sent to
+		$mail->addAddress($recipient, 'John Doe');
+		//Set the subject line
+		$mail->Subject = utf8_decode($subject);
+
+		$mail->isHTML(true);   
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		$mail->msgHTML($message);
+		//Replace the plain text body with one created manually
+		$mail->AltBody = strip_tags($message);
+		//Attach an image file
+		//$mail->addAttachment('images/phpmailer_mini.png');
+		//send the message, check for errors
+		if (!$mail->send()) {
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+		    //echo "Message sent!";
+		}
+	}
+
+	function template($tpl,$data=array())
+	{
+		$str = "";
+		$template = PATH_VIEWS . ( str_replace(".","/",$tpl) ) . '.php';
+
+		if(file_exists($template)){
+			$str= file_get_contents($template);
+		}
+
+		return $str;
+	}    
+}
